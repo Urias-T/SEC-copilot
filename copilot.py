b0eb8e7e6f3866e5d9b7524189fd054905b8b3a5
Urias-T <triumphurias@gmail.com>
2023-12-08 23:10:40 +0100

    made agent

--- copilot.py
+++ copilot.py
@@ -1,8 +1,26 @@
 import os
 
+# For debugging and local experimentation
+
+import langchain
+langchain.debug=True
+
+import yfinance as yf
+
+from pydantic.v1 import BaseModel, Field
+
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
 from langchain.retrievers import KayAiRetriever
+
+from langchain.tools import tool
+from langchain.agents import Tool, initialize_agent
+
+from utils import create_memory
+
+
+class CurrentStockPriceInput(BaseModel):
+    symbol: str = Field(..., description="The ticker symbol for the company whose stock price is to be checked.")
 
 
 def get_response(query, configurations, chat_history):
@@ -11,10 +29,38 @@ def get_response(query, configurations, chat_history):
 
     model = ChatOpenAI(model="gpt-3.5-turbo-16k", openai_api_key=configurations["openai_api_key"])
     retriever = KayAiRetriever.create(dataset_id="company", data_types=["10-K", "10-Q"], num_contexts=6)
-    qa = ConversationalRetrievalChain.from_llm(llm=model, retriever=retriever)
+    qa = RetrievalQA.from_chain_type(llm=model, chain_type="stuff", retriever=retriever)
 
-    result = qa({"question": query, "chat_history": chat_history})
+    memory = create_memory(chat_history=chat_history)
 
-    chat_history.append((query, result["answer"]))
+    @tool(args_schema=CurrentStockPriceInput)
+    def get_current_stock_price(symbol: str) -> str:
+        """Call this function to get the current stock price of a company."""
+        info = yf.Ticker(symbol)
+        current_price = info.info["currentPrice"]
 
-    return result["answer"], chat_history
+        return f"The current price is USD {current_price}"
+    
+
+    retrieval_tool = Tool(
+        name="Kay AI Vector Stor",
+        func=qa.run,
+        description=("Use this tool when answering questions that relate to a company's SEC filings.")
+    )
+
+    tools = [get_current_stock_price, retrieval_tool]
+
+    agent = initialize_agent(
+        agent="chat-conversational-react-description",
+        tools=tools,
+        llm=model,
+        max_iterations=3,
+        early_stopping_method="generate",
+        memory=memory
+    )
+
+    result = agent(query)
+
+    chat_history.append((query, result["output"]))
+
+    return result["output"], chat_history
